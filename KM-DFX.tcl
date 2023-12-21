932178c1fea03e7894ad2a58e9a5811fdbaeecb5
Johan Vansant <johan.vansant@truethrust.com>
2023-12-21 23:30:07 +0100

    patched for ip repo positions

--- KM-DFX.tcl
+++ KM-DFX.tcl
@@ -35,7 +35,7 @@
 proc checkRequiredFiles { origin_dir} {
   set status true
   set files [list \
- "[file normalize "$origin_dir/KM-DFX.srcs/constrs_1/new/pblock_constraints.xdc"]"\
+ "[file normalize "$origin_dir/KM-DFX/srcs/constrs_1/new/pblock_constraints.xdc"]"\
   ]
   foreach ifile $files {
     if { ![file isfile $ifile] } {
@@ -45,8 +45,8 @@ proc checkRequiredFiles { origin_dir} {
   }
 
   set paths [list \
- "[file normalize "$origin_dir/../KM-DFX_RP"]"]"\
- "[file normalize "$origin_dir/../AXI_REG"]"]"\
+ "[file normalize "$origin_dir/KM-DFX_RP"]"]"\
+ "[file normalize "$origin_dir/AXI_REG"]"]"\
   ]
   foreach ipath $paths {
     if { ![file isdirectory $ipath] } {
@@ -136,7 +136,7 @@ if { $validate_required } {
 }
 
 # Create project
-create_project ${_xil_proj_name_} ./${_xil_proj_name_} -part xc7z020clg400-1 -force
+create_project ${_xil_proj_name_} ./${_xil_proj_name_} -part xc7z020clg400-1 
 
 # Set the directory path for the new project
 set proj_dir [get_property directory [current_project]]
@@ -177,7 +177,7 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 # Set IP repository paths
 set obj [get_filesets sources_1]
 if { $obj != {} } {
-   set_property "ip_repo_paths" "[file normalize "$origin_dir/../KM-DFX_RP"] [file normalize "$origin_dir/../AXI_REG"]" $obj
+   set_property "ip_repo_paths" "[file normalize "$origin_dir/KM-DFX_RP"] [file normalize "$origin_dir/AXI_REG"]" $obj
 
    # Rebuild user ip_repo's index before adding any source files
    update_ip_catalog -rebuild
@@ -205,7 +205,7 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
-set file "[file normalize "$origin_dir/KM-DFX.srcs/constrs_1/new/pblock_constraints.xdc"]"
+set file "[file normalize "$origin_dir/KM-DFX/srcs/constrs_1/new/pblock_constraints.xdc"]"
 set file_imported [import_files -fileset constrs_1 [list $file]]
 set file "new/pblock_constraints.xdc"
 set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
